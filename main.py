73f2f7f7344adca12ea0e6ed87befe5c527f91db
Zander <ZanderCeunen@protonmail.com>
2022-12-16 18:06:48 +0100

    Test 1.0.0.2

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from flask import *
-import pytube as yt
+import pytube
 app = Flask(__name__)
-
-
-@app.route('/')
+url = ""
+#yt = pytube.YouTube(video_url)
+#yt.streams.get_highest_resolution().download(path1)
+@app.route('/', methods=["GET", "POST"])
 def start():
+    global url
+    if (request.method == "POST"):
+        url = request.form.get("url")
+        video_url = url
+    return render_template("load.html")
 
-    url = request.form.get("url")
-    return 'hello'
 
 if __name__ == '__main__':
     app.run(host="192.168.0.213")
