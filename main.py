dc57982587bdf65e1eec112581aa0ec39e2f8b85
Zander <ZanderCeunen@protonmail.com>
2022-12-16 17:56:19 +0100

    Test 1.0.0.1

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from flask import *
+import pytube as yt
+app = Flask(__name__)
 
+
+@app.route('/')
+def start():
+
+    url = request.form.get("url")
+    return 'hello'
+
+if __name__ == '__main__':
+    app.run(host="192.168.0.213")
